95b27e4edc9eb540c61226ce7e2923829c832c0c
Thitisak Treerat <krunix3r@users.noreply.github.com>
2022-08-22 17:43:46 +0700

    Update settings.py

--- thitiedu/settings.py
+++ thitiedu/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%65%v@l9vvm3o=ykb*+37vh@m-46%w^&^3gq=(!fwc%=vexhax'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'thitiedu.herokuapp.com']
 
