4501d2d9035246a937fdc92b40c65f65cf5aca82
Thitisak Treerat <krunix3r@users.noreply.github.com>
2022-08-22 15:15:48 +0700

    commit

--- thitiedu/settings.py
+++ thitiedu/settings.py
@@ -51,10 +51,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'thitiedu.urls'
 
+TEMPLATES_URL = Path(BASE_DIR) / 'templates'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_URL],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    Path(BASE_DIR) / "assets",
+    '/var/www/static/',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
